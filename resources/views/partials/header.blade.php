29950e3a09dbdd65a31e681a6d2292310a192799
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-19 17:25:49 +0200

    created scaffolding for scss, added images for project

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,16 @@
 <header>
-    
+    <div class="wrapper">
+        <div class="my_page-logo">
+            <img src="../assets/img/dc-logo.png" alt="DC Main Logo">
+        </div>
+        <nav>
+            <ul class="my_list-nav">
+                <li>
+                    <a></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    <section class="my_hero-section"></section>
 </header>
