1628169d4ced897650870ac30db2417cf9d476ff
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-19 17:51:49 +0200

    imported raw style from header and main section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,21 @@
 @section('title', 'DC Comics')
 
 @section('main-content')
-    <h1>
-        Homepage App
-    </h1>
+    <div class="wrapper wrapper-main">
+        <div class="series-section-title">
+            <h2>Current Series</h2>
+        </div>
+
+        <div class="series-wrapper">
+            <AppCard v-for="card in cards"
+                :thumbnail="card.thumb"
+                :price="card.price"
+                :series="card.series"
+                :type="card.type"/>
+        </div>
+
+        <button type="button" class="btn btn-primary" id="load-more-btn">
+            Load more
+        </button>
+    </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
-    <div class="wrapper">
+    <div class="wrapper wrapper-header">
         <div class="my_page-logo">
-            <img src="../assets/img/dc-logo.png" alt="DC Main Logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Main Logo">
         </div>
         <nav>
             <ul class="my_list-nav">
