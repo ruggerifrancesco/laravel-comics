77a26896b53fb3a98ae94abb0e9071814610576e
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-20 05:05:41 +0200

    compacted comicss array

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comics = config('comics');
+
+    return view('home', compact('comics'));
 });
